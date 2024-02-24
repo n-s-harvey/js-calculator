b89bc05cf4707bbfb5392a01d91ae60f41fd8e03
n-s-harvey <150607031+n-s-harvey@users.noreply.github.com>
2024-02-24 15:40:24 -0600

    Redux Thunk - clear display, dispatch input

---
+++ src/features/Middleware.js
@@ -0,0 +1,31 @@
+// @ts-check
+import React from "react";
+
+import { resetOutput } from "./output/outputSlice";
+import { resetInput } from "./input/inputSlice";
+import { pushInput } from "./input/inputSlice";
+
+/**
+ * @param {string} keypress
+ * @returns {any} - Necessary because TypeScript otherwise believes types are conflicting in KeypadButton.
+ */
+export default function handleKeydown(keypress) {
+  if (keypress == 'clear') {
+    /**
+     * @param {import("@reduxjs/toolkit").Dispatch<import("@reduxjs/toolkit").AnyAction>} dispatch
+     * @param {() => import("@reduxjs/toolkit").Store} getState
+     */
+    return function clearThunk(dispatch, getState) {
+      dispatch(resetInput());
+      dispatch(resetOutput());
+    }
+  }
+
+  /**
+   * @param {import("@reduxjs/toolkit").Dispatch<import("@reduxjs/toolkit").AnyAction>} dispatch
+   * @param {() => import("@reduxjs/toolkit").Store} getState
+   */
+  return function dispatchKey(dispatch, getState) {
+    dispatch(pushInput(keypress));
+  }
+}
