6fef564902ab2c45f309645e80f536edc05f44d9
n-s-harvey <150607031+n-s-harvey@users.noreply.github.com>
2024-02-26 20:30:00 -0600

    Arithmetic operators push working entry to formula line

--- src/features/Middleware.js
+++ src/features/Middleware.js
@@ -6,6 +6,13 @@ import { pushOutput } from "./output/outputSlice";
 import { resetFormula } from "./input/inputSlice";
 import { pushToFormula } from "./input/inputSlice";
 
+const calculatorOperations = new Map([
+  ['*', 'multiply'],
+  ['/', 'divide'],
+  ['+', 'add'],
+  ['-', 'subtract']
+]);
+
 /**
  * @param {string} keypress
  * @returns {any} - Necessary because TypeScript otherwise believes types are conflicting in KeypadButton.
@@ -22,6 +29,22 @@ export default function handleKeydown(keypress) {
     }
   }
 
+  else if (calculatorOperations.has(keypress)) {
+    /**
+     * @param {import("@reduxjs/toolkit").Dispatch<import("@reduxjs/toolkit").AnyAction>} dispatch
+     * @param {() => import("@reduxjs/toolkit").Store} getState
+     */
+    return function pushSymbol(dispatch, getState) {
+      const outputSelector = (state) => state.output.value;
+      const output = outputSelector(getState());
+      if (output !== '0') {
+        dispatch(pushOutput(keypress));
+        const updatedOutput = `${output + keypress}`;
+        dispatch(pushToFormula(updatedOutput));
+        dispatch(resetOutput());
+      }
+    }
+  }
   /**
    * @param {import("@reduxjs/toolkit").Dispatch<import("@reduxjs/toolkit").AnyAction>} dispatch
    * @param {() => import("@reduxjs/toolkit").Store} getState
