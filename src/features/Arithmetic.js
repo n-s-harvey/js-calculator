336e23b3533838336abd013f757821a9948d5e14
n-s-harvey <150607031+n-s-harvey@users.noreply.github.com>
2024-02-29 20:03:10 -0600

    Arithmetic functions

---
+++ src/features/Arithmetic.js
@@ -0,0 +1,71 @@
+/**
+ * @param {Array.<(number|string)>} formula - Formula to evaluate.
+ * @returns {number}
+ */
+export default function simplify(formula) {
+
+  const calculatorOperations = [
+    '*',
+    '/',
+    '+',
+    '-'
+  ];
+
+  if (formula.length == 1 && !isNaN(formula[0])) {
+    let [output] = formula;
+    return output;
+  };
+
+  let lhsIndex, rhsIndex;
+  /**
+   * @type {number}
+   */
+  let operatorIndex;
+  for (const operator of calculatorOperations) {
+    operatorIndex = formula.findIndex(element => element == operator);
+    if (operatorIndex != -1) {
+      lhsIndex = operatorIndex - 1;
+      rhsIndex = operatorIndex + 1;
+      break;
+    }
+  }
+
+  const operator = formula[operatorIndex];
+  const lhs = formula[lhsIndex];
+  const rhs = formula[rhsIndex];
+
+  const result = evaluate(operator, lhs, rhs);
+
+  formula.splice(lhsIndex, 3, result);
+
+  console.log(formula);
+
+  return simplify(formula);
+
+}
+/**
+  * @param {string} op - Operator expressed as a symbol. Expects *, /, +, -
+  * @param {number} lhs - Left hand side of expression
+  * @param {number} rhs - Right hand side of expression
+  */
+function evaluate(op, lhs, rhs) {
+  switch (op) {
+    case '*':
+      return lhs * rhs;
+    case '/':
+      return lhs / rhs;
+    case '+':
+      return lhs + rhs;
+    case '-':
+      return lhs - rhs;
+    default:
+      break;
+  }
+}
+
+// simplify([1, "+", 2])
+// simplify([1, 2])
+// simplify([1, 2, "+", "+"])
+// simplify([10, "*", 5, "+", 2])
+// simplify([5, "+", 11, '*', 100]);
+// console.log(simplify([10, "*", "5", "*", 5]))
