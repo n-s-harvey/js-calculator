c025817378d25b58439bb0fdf15257860dab9bc3
n-s-harvey <150607031+n-s-harvey@users.noreply.github.com>
2024-03-06 09:01:13 -0600

    Operators defined in class to reduce re-definition

---
+++ src/features/Operators.js
@@ -0,0 +1,21 @@
+// @ts-check
+
+/**
+ * Contains properties for multiplication, division, addition, and subtraction.
+ */
+export default class Operators {
+  static multiply = '×';
+  static divide = '÷';
+  static add = '+';
+  static subtract = '-';
+  /**
+    * Array with order of operations. 0 is the highest priority, 3 is the lowest priority.
+    * @type {Array<string>}
+    */
+  static order = [
+    Operators.multiply,
+    Operators.divide,
+    Operators.add,
+    Operators.subtract
+  ];
+}
